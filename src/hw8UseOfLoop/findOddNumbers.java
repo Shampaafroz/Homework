81a775d812879d5991b43b8e924fe839e73f37c9
shampaafroz <shampa.afroz82@gmail.com>
2024-04-29 22:54:21 -0400

    loops

---
+++ src/hw8UseOfLoop/findOddNumbers.java
@@ -0,0 +1,14 @@
+package hw8UseOfLoop;
+
+public class findOddNumbers {
+	public static void main(String[] args) {
+		for (int i = -20; i <= 50; i++) {
+			if (i % 2 != 0) {
+				System.out.println("Odd Number: " + i);
+
+			}
+		}
+
+	}
+
+}
